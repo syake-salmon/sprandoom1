c61bf4844bd7083b1c990db470223e556febd173
ASAMI Keisuke <syakeApps@icloud.com>
2022-08-11 15:16:20 +0900

    リファクタリング

--- src/main/java/com/syakeapps/sprandoom1/view/bean/ApplicationContext.java
+++ src/main/java/com/syakeapps/sprandoom1/view/bean/ApplicationContext.java
@@ -11,32 +11,35 @@ import javax.persistence.PersistenceContext;
 
 import com.syakeapps.sprandoom1.jpa.bean.Special;
 import com.syakeapps.sprandoom1.jpa.bean.Sub;
+import com.syakeapps.sprandoom1.jpa.bean.Weapon;
 import com.syakeapps.sprandoom1.jpa.bean.WeaponClass;
 
 @ApplicationScoped
 @Named(value = "APP")
 public class ApplicationContext {
-    public static final String PERSISTENCE_UNIT_NAME = "sprandoom1PU";
-
     private static final Logger LOGGER = Logger.getLogger(ApplicationContext.class.getName());
 
-    @PersistenceContext(unitName = PERSISTENCE_UNIT_NAME)
+    @PersistenceContext
     private EntityManager em;
 
     /* CONST */
+    private List<Weapon> weapons;
     private List<WeaponClass> classes;
-
     private List<Sub> subs;
-
     private List<Special> specials;
 
     @PostConstruct
     private void initialize() {
         LOGGER.info("Initializing application.");
+        weapons = em.createNamedQuery(Weapon.FIND_ALL, Weapon.class).getResultList();
         classes = em.createNamedQuery(WeaponClass.FIND_ALL, WeaponClass.class).getResultList();
         subs = em.createNamedQuery(Sub.FIND_ALL, Sub.class).getResultList();
         specials = em.createNamedQuery(Special.FIND_ALL, Special.class).getResultList();
         LOGGER.info("Initializing application is done.");
+    }
+
+    public List<Weapon> getWeapons() {
+        return weapons;
     }
 
     public List<WeaponClass> getClasses() {
