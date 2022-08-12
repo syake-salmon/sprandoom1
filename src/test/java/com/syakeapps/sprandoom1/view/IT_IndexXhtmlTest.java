34ec07f543828ca39021be049a144028b9dce4c7
ASAMI Keisuke <syakeApps@icloud.com>
2022-08-12 20:44:21 +0900

    linux ChromeDriver lang option not working

--- src/test/java/com/syakeapps/sprandoom1/view/IT_IndexXhtmlTest.java
+++ src/test/java/com/syakeapps/sprandoom1/view/IT_IndexXhtmlTest.java
@@ -88,7 +88,7 @@ public class IT_IndexXhtmlTest extends Arquillian {
         for (int i = 0; i < locales.size(); i++) {
             Locale locale = locales.get(i);
 
-            options.add("--lang=" + locale.getLanguage());
+            options.add("--lang=" + locale.getLanguage() + "-" + locale.getCountry());
             WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments(options));
 
             ResourceBundle.Control control = ResourceBundle.Control
